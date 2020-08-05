3a504f6a2d5c11cfe2f8cf12bcb5f400d08974dd
fongwz <schifx95@gmail.com>
2020-08-05 18:30:04 +0800

    PANGKU

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './pangku.PNG';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Image } from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Image src={logo} className="App-logo" alt="logo" size='medium' circular/>
+        <br/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WUB WUB WUB WUBBBB
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
