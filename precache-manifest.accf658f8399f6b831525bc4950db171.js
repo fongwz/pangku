f25af2555ef58e201402cf9e3fd33a2c7ddbe84f
fongwz <schifx95@gmail.com>
2020-08-05 15:55:55 +0800

    Updates

---
+++ precache-manifest.accf658f8399f6b831525bc4950db171.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00241094918759048b6a180e7b54661",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "edcf2b90ac2f811b2898",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "38a083b953ccef603ce4",
+    "url": "/my-app/static/js/2.1f7cd0f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.1f7cd0f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edcf2b90ac2f811b2898",
+    "url": "/my-app/static/js/main.2989ef76.chunk.js"
+  },
+  {
+    "revision": "b610a9f2b8ffc29c76ef",
+    "url": "/my-app/static/js/runtime-main.17e7377c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
